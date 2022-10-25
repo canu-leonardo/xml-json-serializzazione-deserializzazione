dfae9d833cb422a8b751b97e7945b8dfecea1c49
lai2-01 <lai2-01@itismeucci.com>
2022-10-25 09:33:40 +0200

    finita la serializzazione e deserializzazione anche in formato json

---
+++ Serializza_deserializza_toJson/serializza-deserializza/src/main/java/com/itismeucci/it/App.java
@@ -0,0 +1,64 @@
+package com.itismeucci.it;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+public class App 
+{ 
+    private static void serializza(Classe c){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            objectMapper.writeValue(new File ("miaclasse.json"), c);
+        } catch (Exception e) {
+            System.out.println("errore nel file");
+        }
+    }
+
+    private static Classe deserializza(String nomeFile){
+        Classe miaClasse = new Classe();
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            miaClasse = objectMapper.readValue(new File(nomeFile), Classe.class);
+        } catch (Exception e) {
+            System.out.println("errore nel leggere il file");
+        }
+
+        return miaClasse;
+    }
+    
+    
+    public static void main( String[] args )
+    {
+        Date data = new Date(2004, 1, 2);
+        Date data1 = new Date(2004, 2, 3);
+        Date data2 = new Date(2004, 3, 4);
+        Date data3 = new Date(2004, 4, 5);
+        Date data4 = new Date(2004, 5, 6);
+        Alunno alunno1 = new Alunno("marco", "marco", data );
+        Alunno alunno2 = new Alunno("gianni", "gianni", data1 );
+        Alunno alunno3 = new Alunno("franco", "franco", data2 );
+        Alunno alunno4 = new Alunno("mario", "mario", data3 );
+        Alunno alunno5 = new Alunno("luigi", "luigi", data4 );
+
+        ArrayList <Alunno> alunni = new ArrayList<>();
+        alunni.add(alunno1);
+        alunni.add(alunno2);
+        alunni.add(alunno3);
+        alunni.add(alunno4);
+        alunni.add(alunno5);
+
+        Classe classe = new Classe(5, "DIA", "10-TW", alunni);
+
+        serializza(classe);
+
+        Classe classe_ritornata = deserializza("miaclasse.json");
+        
+        System.out.println(classe_ritornata.toString());
+
+        
+    }
+}
